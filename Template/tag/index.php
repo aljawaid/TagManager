b90074aa14b9480323ec9c55f92255b783057bbd
aljawaid <10233708+aljawaid@users.noreply.github.com>
2022-12-16 04:24:36 +0000

    Show Colour with Colour Name
    - WIP #2

--- Template/tag/index.php
+++ Template/tag/index.php
@@ -13,7 +13,7 @@
     <p class="tag-title-text">
         <?= t('The tags in this list are global and available for any project. Use the CSS selectors for further styling. These tags are also not included in any project tag counts.') ?>
     </p>
-    <table class="table-striped table-scrolling">
+    <table class="project-tags-table table-striped table-scrolling">
         <thead>
             <tr class="">
                 <th class="column-60"><?= t('Tag') ?></th>
@@ -25,7 +25,7 @@
         <?php foreach ($tags as $tag): ?>
             <tr class="">
                 <td class=""><?= $this->text->e($tag['name']) ?></td>
-                <td class=""><?= $this->text->e($colors[$tag['color_id']] ?? '') ?></td>
+                <td class="text-center task-tag <?= $tag['color_id'] ? "color-{$tag['color_id']}" : '' ?>"><?= $this->text->e($colors[$tag['color_id']] ?? '') ?></td>
                 <td class="">
                     <?= $this->modal->medium('edit', t('Edit'), 'TagController', 'edit', array('tag_id' => $tag['id'])) ?>
                     <?= $this->modal->confirm('trash-o', t('Delete'), 'TagController', 'confirm', array('tag_id' => $tag['id'])) ?>
