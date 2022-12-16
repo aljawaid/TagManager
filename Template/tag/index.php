b552c01a4b40b5d190c7d758db9b8c5920cffc19
aljawaid <10233708+aljawaid@users.noreply.github.com>
2022-12-16 04:04:29 +0000

    Improve HTML Table Structure
    - WIP #2

--- Template/tag/index.php
+++ Template/tag/index.php
@@ -11,20 +11,24 @@
     <p class="alert"><?= t('There is no global tag at the moment.') ?></p>
 <?php else: ?>
     <table class="table-striped table-scrolling">
-        <tr>
-            <th class="column-60"><?= t('Tag') ?></th>
-            <th class="column-20"><?= t('Color') ?></th>
-            <th><?= t('Action') ?></th>
-        </tr>
+        <thead>
+            <tr class="">
+                <th class="column-60"><?= t('Tag') ?></th>
+                <th class="column-20"><?= t('Color') ?></th>
+                <th><?= t('Action') ?></th>
+            </tr>
+        </thead>
+        <tbody>
         <?php foreach ($tags as $tag): ?>
-            <tr>
-                <td><?= $this->text->e($tag['name']) ?></td>
-                <td><?= $this->text->e($colors[$tag['color_id']] ?? '') ?></td>
-                <td>
+            <tr class="">
+                <td class=""><?= $this->text->e($tag['name']) ?></td>
+                <td class=""><?= $this->text->e($colors[$tag['color_id']] ?? '') ?></td>
+                <td class="">
                     <?= $this->modal->medium('edit', t('Edit'), 'TagController', 'edit', array('tag_id' => $tag['id'])) ?>
                     <?= $this->modal->confirm('trash-o', t('Remove'), 'TagController', 'confirm', array('tag_id' => $tag['id'])) ?>
                 </td>
             </tr>
         <?php endforeach ?>
+        </tbody>
     </table>
 <?php endif ?>
