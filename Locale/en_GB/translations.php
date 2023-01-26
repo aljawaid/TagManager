27af3062753d38f0a222026e18fabceab16a6c71
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-01-26 21:04:54 +0000

    FIX: Unix (LF) Line Endings and Tabs > Spaces

--- Locale/en_GB/translations.php
+++ Locale/en_GB/translations.php
@@ -1,9 +1,10 @@
 <?php
+
 return array(
-  //
-  // GENERAL
-  //
-  'Tags management' => 'Tag Manager',
-  'Global tags management' => 'Tag Manager',
-  'Project tags management' => 'Project Tags',
+    //
+    // GENERAL
+    //
+    'Tags management' => 'Tag Manager',
+    'Global tags management' => 'Tag Manager',
+    'Project tags management' => 'Project Tags',
 );
--- Locale/translations-starter-template.php
+++ Locale/translations-starter-template.php
@@ -1,58 +1,59 @@
 <?php
+
 return array(
-  //
-  // GENERAL
-  //
-  'Use tags like you have never used them before. This plugin revamps the whole project and global tags concept to provide the user with a clearer process of creating and associating tags to better improve their workflow.' => '',
-  'Tags management' => 'Tag Manager',
-  'Global tags management' => 'Tag Manager',
-  'Project tags management' => 'Project Tags',
-  //
-  // project_tag/index.php
-  //
-  'Project Tags' => '',
-  'Project' => '',
-  'Tags' => '',
-  'Add Project Tag' => '',
-  'Global' => '',
-  'Global Tags Available' => '',
-  'Add Global Tag' => '',
-  'Enable global tags for this project' => '',
-  'There is no specific tag for this project at the moment.' => '',
-  'The tags in this list are specific to this project. Use the CSS selectors for further styling.' => '',
-  'Tag' => '',
-  'Colour' => '',
-  'Tag ID' => '',
-  'HTML/CSS ID' => '',
-  'HTML/CSS Class' => '',
-  'Actions' => '',
-  'HTML' => '',
-  'CSS' => '',
-  'Edit' => '',
-  'Convert' => '',
-  'Delete' => '',
-  'Manage Global Tags' => '',
-  'Global Tags' => '',
-  'The tags in this list are global and available for any project. Use the CSS selectors for further styling. These tags are not included in your project tag count.' => '',
-  'There are no global tags at the moment.' => '',
-  //
-  // task/details.php
-  //
-  'Associated Tags' => '',
-  //
-  // project_header/dropdown.php
-  //
-  'Go to Global Tags' => '',
-  //
-  // tagfilter.php
-  //
-  'Tag Filters' => '',
-  'No Tag' => '',
-  //
-  // tag/index.php
-  //
-  'Global Tags' => '',
-  'Available Global Tags' => '',
-  'There is no global tag at the moment.' => '',
-  'The tags in this list are global and available for any project. Use the CSS selectors for further styling. These tags are also not included in any project tag counts.' => '',
+    //
+    // GENERAL
+    //
+    'Use tags like you have never used them before. This plugin revamps the whole project and global tags concept to provide the user with a clearer process of creating and associating tags to better improve their workflow.' => '',
+    'Tags management' => 'Tag Manager',
+    'Global tags management' => 'Tag Manager',
+    'Project tags management' => 'Project Tags',
+    //
+    // project_tag/index.php
+    //
+    'Project Tags' => '',
+    'Project' => '',
+    'Tags' => '',
+    'Add Project Tag' => '',
+    'Global' => '',
+    'Global Tags Available' => '',
+    'Add Global Tag' => '',
+    'Enable global tags for this project' => '',
+    'There is no specific tag for this project at the moment.' => '',
+    'The tags in this list are specific to this project. Use the CSS selectors for further styling.' => '',
+    'Tag' => '',
+    'Colour' => '',
+    'Tag ID' => '',
+    'HTML/CSS ID' => '',
+    'HTML/CSS Class' => '',
+    'Actions' => '',
+    'HTML' => '',
+    'CSS' => '',
+    'Edit' => '',
+    'Convert' => '',
+    'Delete' => '',
+    'Manage Global Tags' => '',
+    'Global Tags' => '',
+    'The tags in this list are global and available for any project. Use the CSS selectors for further styling. These tags are not included in your project tag count.' => '',
+    'There are no global tags at the moment.' => '',
+    //
+    // task/details.php
+    //
+    'Associated Tags' => '',
+    //
+    // project_header/dropdown.php
+    //
+    'Go to Global Tags' => '',
+    //
+    // tagfilter.php
+    //
+    'Tag Filters' => '',
+    'No Tag' => '',
+    //
+    // tag/index.php
+    //
+    'Global Tags' => '',
+    'Available Global Tags' => '',
+    'There is no global tag at the moment.' => '',
+    'The tags in this list are global and available for any project. Use the CSS selectors for further styling. These tags are also not included in any project tag counts.' => '',
 );
